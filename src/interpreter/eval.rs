23e3d74359ef487b54b84e350502c8357891ebc3
bogo8liuk <lucaborghi99@gmail.com>
2025-01-06 10:59:56 +0100

    making result of evaluation fallible

--- src/interpreter/eval.rs
+++ src/interpreter/eval.rs
@@ -1,3 +1,8 @@
+use std::{
+    fmt::Display,
+    ops::{Add, Div, Mul, Neg, Rem, Sub},
+};
+
 use tokio::{
     sync::{mpsc, oneshot},
     task::JoinHandle,
@@ -7,12 +12,24 @@ use crate::interpreter::{ast::*, context::NamesStack};
 
 type ToFlush = String;
 
+#[derive(Debug)]
+struct ProcError;
+
+impl Display for ProcError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "*** Process error ***")
+    }
+}
+
 pub async fn eval_and_flush(process: Process) {
     //let names_stack = NamesStack::new();
     let names_stack_handle = StackHandle::new();
-    let to_flush = eval_process(Box::new(process), names_stack_handle).await;
-    let to_flush_str = to_flush.as_str();
-    println!("{}", to_flush_str)
+    let to_flush_res = eval_process(Box::new(process), names_stack_handle).await;
+
+    match to_flush_res {
+        Ok(to_flush) => println!("{}", to_flush.as_str()),
+        Err(err) => println!("{}", err),
+    }
 }
 
 /* The runtime context is handled with the actor pattern. No shared data, just
@@ -21,14 +38,23 @@ stack). Requests (and responses) are implemented through message passing. This
 could be sub-optimal (???), but it avoids the headaches that could cause things
 like Arc + RwLock _et similia_, sources of ownership and lifetime issues,
 especially with async recursion. */
-async fn eval_process(process: Box<Process>, names_stack_handle: StackHandle) -> ToFlush {
+async fn eval_process(
+    process: Box<Process>,
+    names_stack_handle: StackHandle,
+) -> Result<ToFlush, ProcError> {
     match *process {
         Process::Expr(expr) => eval_expr(&expr),
         Process::Loop(proc) => {
             loop {
                 let handle_clone = names_stack_handle.clone();
                 // Ast cloning is probably actually sub-optimal
-                Box::pin(eval_process(proc.clone(), handle_clone)).await;
+                let res = Box::pin(eval_process(proc.clone(), handle_clone)).await;
+
+                match res {
+                    Ok(_) => (),
+                    // Break on errors
+                    Err(_) => return res,
+                }
             }
             //"".to_owned()
         }
@@ -43,7 +69,7 @@ async fn eval_process(process: Box<Process>, names_stack_handle: StackHandle) ->
             fn eval_wrap(
                 process: Box<Process>,
                 names_stack_handle: StackHandle,
-            ) -> JoinHandle<ToFlush> {
+            ) -> JoinHandle<Result<ToFlush, ProcError>> {
                 tokio::spawn(eval_process(process, names_stack_handle))
             }
             let handle_clone = names_stack_handle.clone();
@@ -53,7 +79,11 @@ async fn eval_process(process: Box<Process>, names_stack_handle: StackHandle) ->
             let res1 = join1.await.unwrap();
             let res2 = join2.await.unwrap();
 
-            ["<", res1.as_str(), ",", res2.as_str(), ">"].concat()
+            match (res1, res2) {
+                (Ok(v1), Ok(v2)) => Ok(["<", v1.as_str(), ",", v2.as_str(), ">"].concat()),
+                (Err(e1), _) => Err(e1),
+                (_, Err(e2)) => Err(e2),
+            }
         }
         Process::Send(_, _, _) => todo!(),
         Process::Receive(_, _, _) => todo!(),
@@ -121,10 +151,10 @@ impl StackHandle {
     }
 }
 
-fn eval_expr(expr: &Expression) -> ToFlush {
+fn eval_expr(expr: &Expression) -> Result<ToFlush, ProcError> {
     match expr {
-        Expression::Val(val) => eval_value(val),
-        Expression::IntExpr(expr) => eval_int_expr(expr).to_string(),
+        Expression::Val(val) => Ok(eval_value(val)),
+        Expression::IntExpr(expr) => eval_int_expr(expr).map(|n| n.to_string()).into(),
     }
 }
 
@@ -141,7 +171,113 @@ fn eval_value(val: &Value) -> ToFlush {
     }
 }
 
-fn eval_int_expr(expr: &IntExpr) -> i32 {
+struct ExprResult<T>(Result<T, ProcError>);
+
+impl<T> ExprResult<T> {
+    fn map<B>(self, f: impl FnOnce(T) -> B) -> ExprResult<B> {
+        match self {
+            ExprResult(Ok(x)) => ExprResult(Ok(f(x))),
+            ExprResult(Err(err)) => ExprResult(Err(err)),
+        }
+    }
+
+    fn to_result(self) -> Result<T, ProcError> {
+        let ExprResult(res) = self;
+        res
+    }
+}
+
+impl<T> From<T> for ExprResult<T> {
+    fn from(value: T) -> Self {
+        ExprResult(Ok(value))
+    }
+}
+
+impl<T> From<Result<T, ProcError>> for ExprResult<T> {
+    fn from(value: Result<T, ProcError>) -> Self {
+        ExprResult(value)
+    }
+}
+
+impl<T> Into<Result<T, ProcError>> for ExprResult<T> {
+    fn into(self) -> Result<T, ProcError> {
+        self.to_result()
+    }
+}
+
+impl<T: Add> Add for ExprResult<T> {
+    type Output = ExprResult<<T as Add>::Output>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (ExprResult(Ok(n1)), ExprResult(Ok(n2))) => ExprResult(Ok(n1 + n2)),
+            (ExprResult(Err(e1)), _) => ExprResult(Err(e1)),
+            (_, ExprResult(Err(e2))) => ExprResult(Err(e2)),
+        }
+    }
+}
+
+impl<T: Sub> Sub for ExprResult<T> {
+    type Output = ExprResult<<T as Sub>::Output>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (ExprResult(Ok(n1)), ExprResult(Ok(n2))) => ExprResult(Ok(n1 - n2)),
+            (ExprResult(Err(e1)), _) => ExprResult(Err(e1)),
+            (_, ExprResult(Err(e2))) => ExprResult(Err(e2)),
+        }
+    }
+}
+
+impl<T: Mul> Mul for ExprResult<T> {
+    type Output = ExprResult<<T as Mul>::Output>;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (ExprResult(Ok(n1)), ExprResult(Ok(n2))) => ExprResult(Ok(n1 * n2)),
+            (ExprResult(Err(e1)), _) => ExprResult(Err(e1)),
+            (_, ExprResult(Err(e2))) => ExprResult(Err(e2)),
+        }
+    }
+}
+
+impl<T: Div> Div for ExprResult<T> {
+    type Output = ExprResult<<T as Div>::Output>;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            // TODO: check for division by 0 ???
+            (ExprResult(Ok(n1)), ExprResult(Ok(n2))) => ExprResult(Ok(n1 / n2)),
+            (ExprResult(Err(e1)), _) => ExprResult(Err(e1)),
+            (_, ExprResult(Err(e2))) => ExprResult(Err(e2)),
+        }
+    }
+}
+
+impl<T: Rem> Rem for ExprResult<T> {
+    type Output = ExprResult<<T as Rem>::Output>;
+
+    fn rem(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (ExprResult(Ok(n1)), ExprResult(Ok(n2))) => ExprResult(Ok(n1 % n2)),
+            (ExprResult(Err(e1)), _) => ExprResult(Err(e1)),
+            (_, ExprResult(Err(e2))) => ExprResult(Err(e2)),
+        }
+    }
+}
+
+impl<T: Neg> Neg for ExprResult<T> {
+    type Output = ExprResult<<T as Neg>::Output>;
+
+    fn neg(self) -> Self::Output {
+        match self {
+            ExprResult(Ok(n)) => ExprResult(Ok(-n)),
+            ExprResult(Err(e)) => ExprResult(Err(e)),
+        }
+    }
+}
+
+fn eval_int_expr(expr: &IntExpr) -> ExprResult<i32> {
     match expr {
         IntExpr::Add(e1, e2) => eval_int_expr(e1) + eval_int_expr(e2),
         IntExpr::Sub(e1, e2) => eval_int_expr(e1) - eval_int_expr(e2),
@@ -149,7 +285,7 @@ fn eval_int_expr(expr: &IntExpr) -> i32 {
         IntExpr::Div(e1, e2) => eval_int_expr(e1) / eval_int_expr(e2),
         IntExpr::Mod(e1, e2) => eval_int_expr(e1) % eval_int_expr(e2),
         IntExpr::Neg(e) => -eval_int_expr(e),
-        IntExpr::Lit(i) => *i,
+        IntExpr::Lit(i) => (*i).into(),
     }
 }
 
